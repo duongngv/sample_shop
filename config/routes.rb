793fefe3e5f03ee93d1ee5c7d9778b4222a367a2
Duong Nguyen <nv.duongnv@gmail.com>
2019-08-28 10:21:08 +0700

    Filling on layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    get "/home", to: "static_pages#home"
+    get "/about", to: "static_pages#about"
+    root "static_pages#home"
+  end
 end
